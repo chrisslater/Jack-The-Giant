02e3a335c30ec283c5b7fba9e58f4716a2351b35
Christopher Slater <chris@fourzerofour.uk>
2016-04-16 18:01:55 +0100

    Add positioned clouds to the Game

---
+++ Assets/Scripts/Cloud Collectors Scripts/CloudSpawner.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudSpawner : MonoBehaviour {
+
+
+	[SerializeField]
+	private GameObject[] clouds;
+
+	[SerializeField]
+	private GameObject[] collectables;
+
+	private GameObject player;
+
+	private float distanceBetweenTheClouds = 3f;
+	private float minX;
+	private float maxX;
+	private float lastCloudPositionY;
+	private float controlX;
+
+	void Awake () {
+		controlX = 0;
+		SetMinAndMaxX ();
+		CreateClouds ();
+	}
+
+	void SetMinAndMaxX () {
+		Vector3 bounds = Camera.main.ScreenToWorldPoint (
+			new Vector3(Screen.width, Screen.height, 0)
+		);
+
+		maxX = bounds.x - 0.5f;
+		minX = -bounds.x + 0.5f;
+	}
+
+	void Shuffle (GameObject[] arrayToShuffle) {
+		for (int i = 0; i < arrayToShuffle.Length; i++) {
+			GameObject temp = arrayToShuffle [i];
+			int random = Random.Range (i, arrayToShuffle.Length);
+			arrayToShuffle [i] = arrayToShuffle [random];
+			arrayToShuffle [random] = temp;
+		}
+	}
+
+	void CreateClouds () {
+		Shuffle (clouds);
+
+		float positionY = 0f;
+
+		for (int i = 0; i < clouds.Length; i++) {
+			Vector3 temp = clouds [i].transform.position;
+			temp.y = positionY;
+
+			if (controlX == 0) {
+				temp.x = Random.Range (0.0f, maxX);
+				controlX = 1;
+			} else if (controlX == 1) {
+				temp.x = Random.Range (0.0f, minX);
+				controlX = 2;
+			} else if (controlX == 2) {
+				temp.x = Random.Range (1.0f, maxX);
+				controlX = 3;
+			} else if (controlX == 3) {
+				temp.x = Random.Range (-1.0f, minX);
+				controlX = 0;
+			}
+
+
+			lastCloudPositionY = positionY;
+			clouds [i].transform.position = temp;
+			positionY -= distanceBetweenTheClouds;
+		}
+	}
+}
--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -6,13 +6,51 @@ public class Player : MonoBehaviour {
 	public float speed = 8f;
 	public float maxVelocity = 4f;
 
+	private Rigidbody2D myBody;
+	private Animator anim;
+
+	void Awake () {
+		myBody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		PlayerMoveKeyboard ();
+	}
+
+	void PlayerMoveKeyboard () {
+		float forceX = 0f;
+		float velocity = Mathf.Abs (myBody.velocity.x);
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+
+		if (horizontal > 0) {
+			if (velocity < maxVelocity) {
+				forceX = speed;
+				anim.SetBool ("Walk", true);
+
+				Vector3 temp = transform.localScale;
+				temp.x = 1.3f;
+				transform.localScale = temp;
+			}
+		} else if (horizontal < 0) {
+			if (velocity < maxVelocity) {
+				forceX = -speed;
+				anim.SetBool ("Walk", true);
+
+				Vector3 temp = transform.localScale;
+				temp.x = -1.3f;
+				transform.localScale = temp;
+			}
+		} else {
+			anim.SetBool ("Walk", false);
+		}
+
+		myBody.AddForce (new Vector2(forceX, 0));
 	}
 }
