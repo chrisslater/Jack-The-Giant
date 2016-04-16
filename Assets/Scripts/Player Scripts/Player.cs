3aa6743540f2a4c90b7807488157f2012c5a1d95
Christopher Slater <chris@fourzerofour.uk>
2016-04-16 14:44:55 +0100

    Add project to version control

---
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 8f;
+	public float maxVelocity = 4f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
